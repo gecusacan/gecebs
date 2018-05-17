fb9370be7192597fac8dea6974a852e28cad047b
gecusacan <39341052+gecusacan@users.noreply.github.com>
2018-05-17 00:34:03 -0400

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -13,21 +13,38 @@ with open ('data.txt') as f:
 		print(line)
 f.close()
 
-print('==================================')
+print('===========================================================')
 with open("data.txt", "r") as f:
 	data = f.readlines()
-	print("EXAMPLE curl command for each row in the wells sensor data file")
+	print("EXAMPLE curl command for each row in the Well's sensor data file")
 	print("FYI: wellshipment (i.e. actual Well #1) is hard-coded to the value WSHIP_001 for now")
-	print('==================================')
+	print('===========================================================')
 	for line in data:
-		words = line.split()
-		print words
+		data = line.split()
+		#print row
+		print data
 		#print ("curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ \"$class\": \"org.gec.ebs.SensorReading\", \"fieldtimePTorNPTFlagYesNo\": \"PT\", \"fieldTimeStamp\": \"2018-05-14T20:17:19.340Z\", \"wellshipment\": \"WSHIP_001\", \"timestamp\": \"2018-05-14T20:17:19.340Z\" }' 'http://localhost:3000/api/org.gec.ebs.SensorReading'")
+		##print row elements
+		#print data[0]
+		#print data[1]
 		
-		shipment = "\"" + 'WSHIP_001' + "\""
-		# TODO: convert 0,1 or -1 to PT NPT or UNK
-		# do IF statement
-		timeIndicator = "\"" + "UNK" + "\""
+		# TODO: convert 0,1 or -1 to PT NPT or UNK	
+		#default Error value that will be rejected
+		timeIndicator = "\"" + "XX" + "\""
+		
+		if data[1] == '0':
+			timeIndicator = "\"" + "UNK" + "\""
+		if data[1] == '-1':
+			timeIndicator = "\"" + "NPT" + "\""
+		if data[1] == '1':
+			timeIndicator = "\"" + "PT" + "\""
+
+		wellId = "\"" + 'WELL_001' + "\""
+
+		# hard-coded for now
+		##timeIndicator = "\"" + "UNK" + "\""
 		
 		#print ("curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ \"$class\": \"org.gec.ebs.SensorReading\", \"fieldtimePTorNPTFlagYesNo\": \"PT\", \"fieldTimeStamp\": \"2018-05-14T20:17:19.340Z\", \"wellshipment\": " + shipment + ", \"timestamp\": \"2018-05-14T20:17:19.340Z\" }' 'http://localhost:3000/api/org.gec.ebs.SensorReading'")
-		print ("curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ \"$class\": \"org.gec.ebs.SensorReading\", \"fieldtimePTorNPTFlagYesNo\": " + timeIndicator + ", \"fieldTimeStamp\": \"2018-05-14T20:17:19.340Z\", \"wellshipment\": " + shipment + ", \"timestamp\": \"2018-05-14T20:17:19.340Z\" }' 'http://localhost:3000/api/org.gec.ebs.SensorReading'")
+		#print ("curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ \"$class\": \"org.gec.ebs.SensorReading\", \"fieldtimePTorNPTFlagYesNo\": " + timeIndicator + ", \"fieldTimeStamp\": \"2018-05-14T20:17:19.340Z\", \"wellshipment\": " + shipment + ", \"timestamp\": \"2018-05-14T20:17:19.340Z\" }' 'http://localhost:3000/api/org.gec.ebs.SensorReading'")
+		
+		print ("curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ \"$class\": \"org.gec.ebs.SensorReading\", \"fieldtimePTorNPTFlagYesNo\": " + timeIndicator + ", \"fieldTimeStamp\": \"2018-05-14T20:17:19.340Z\", \"wellId\": " + wellId + ", \"timestamp\": \"2018-05-14T20:17:19.340Z\" }' 'http://localhost:3000/api/org.gec.ebs.SensorReading'")
